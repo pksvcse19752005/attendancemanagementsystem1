2c52f01b5f7c9acf1c20dc2496a37018689004d8
pksvcse19752005 <vinaypydi6@gmail.com>
2025-09-06 10:43:44 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -21,7 +21,7 @@ EMAIL_PASSWORD = "pxbntsohbnbojhtw"  # Use your app password securely
 
 @app.route('/')
 def home():
-    return render_template('attendance.html')
+    return render_template('attendance1.html')
 
 @app.route('/reset-password')
 def reset_password():
@@ -122,4 +122,4 @@ def export_absentees():
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
-            
+    
